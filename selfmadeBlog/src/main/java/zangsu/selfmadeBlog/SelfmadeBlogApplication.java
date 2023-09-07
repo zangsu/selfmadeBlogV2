e61f563053dacfda911a97cc81c01e2bf8414307
zangsu <zangsu_@naver.com>
2023-09-07 20:44:03 +0900

    init : make project

---
+++ selfmadeBlog/src/main/java/zangsu/selfmadeBlog/SelfmadeBlogApplication.java
@@ -0,0 +1,13 @@
+package zangsu.selfmadeBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelfmadeBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelfmadeBlogApplication.class, args);
+	}
+
+}
