30ab584529d91896d6203be0aecdd8c95c16caa0
zangsu <zangsu_@naver.com>
2023-09-17 16:16:09 +0900

    config : change jenkins server to window

--- src/main/java/zangsu/selfmadeBlog/temp/TempChange.java
+++ src/main/java/zangsu/selfmadeBlog/temp/TempChange.java
@@ -1,9 +1,7 @@
 package zangsu.selfmadeBlog.temp;
 
 public class TempChange {
-    //temp change
+    //Winodw 환경에서 다시 Jenkins 연결
 
-    //Jenkins에서 gradle 버전 변경(8.2.1)
 
-    //디렉토리 depth를 1단계 낮춤
 }
