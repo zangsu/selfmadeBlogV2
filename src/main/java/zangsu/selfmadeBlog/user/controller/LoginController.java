b4a95fb39a2a90cd7edcddd6b7c54ac376ef5097
zangsu <zangsu_@naver.com>
2023-12-23 21:01:13 +0900

    feat : 로그인 로직 (세션 O) 완료

--- src/main/java/zangsu/selfmadeBlog/user/controller/LoginController.java
+++ src/main/java/zangsu/selfmadeBlog/user/controller/LoginController.java
@@ -1,5 +1,8 @@
 package zangsu.selfmadeBlog.user.controller;
 
+import java.net.http.HttpRequest;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import zangsu.selfmadeBlog.exception.SmbException;
 import zangsu.selfmadeBlog.user.controller.model.LoginDTO;
+import zangsu.selfmadeBlog.user.controller.model.UserNameDTO;
 import zangsu.selfmadeBlog.user.repository.model.DBUser;
 import zangsu.selfmadeBlog.user.service.LoginService;
 import zangsu.selfmadeBlog.user.service.UserService;
@@ -21,6 +25,7 @@ import zangsu.selfmadeBlog.user.service.model.ServiceUser;
 @RequestMapping("/login")
 @RequiredArgsConstructor
 public class LoginController {
+    private static final String SESSION_KEY = "loginUser";
 
     @Autowired
     private final LoginService loginService;
@@ -28,24 +33,34 @@ public class LoginController {
     private final UserService userService;
 
     @GetMapping("/")
-    public String loginForm(@ModelAttribute(name = "loginDTO") LoginDTO loginDTO) {
+    public String loginForm(@ModelAttribute(name = "loginDTO") LoginDTO loginDTO,
+                            @ModelAttribute(name = "userNameDTO") UserNameDTO nameDTO,
+                            HttpServletRequest request) {
+
+        HttpSession loginSession = request.getSession(false);
+        if (loginSession != null && loginSession.getAttribute(SESSION_KEY) != null) {
+            ServiceUser loginUser = (ServiceUser) loginSession.getAttribute(SESSION_KEY);
+            nameDTO.setName(loginUser.getUserName());
+            return "login/loginHome";
+        }
         return "login/loginForm";
     }
 
     @PostMapping("/login")
     public String login(@ModelAttribute LoginDTO loginDTO,
                         BindingResult bindingResult,
-                        @ModelAttribute(name = "serviceUser") ServiceUser user) {
-
+                        @ModelAttribute(name = "userNameDTO") UserNameDTO nameDTO,
+                        HttpServletRequest request) {
         try {
             long loginIndex = loginService.login(loginDTO.getLonginId(), loginDTO.getPassword());
             ServiceUser result = userService.findUser(loginIndex);
-            user.setUserName(result.getUserName());
-
+            nameDTO.setName(result.getUserName());
+            HttpSession session = request.getSession();
+            session.setAttribute(SESSION_KEY, result);
+            return "login/loginHome";
         } catch (SmbException e){
             bindingResult.reject("loginFail", "아이디 또는 비밀번호가 잘못되었습니다");
             return "login/loginForm";
         }
-        return "login/loginHome";
     }
 }
---
+++ src/main/java/zangsu/selfmadeBlog/user/controller/model/UserNameDTO.java
@@ -0,0 +1,8 @@
+package zangsu.selfmadeBlog.user.controller.model;
+
+import lombok.Data;
+
+@Data
+public class UserNameDTO {
+    private String name;
+}
