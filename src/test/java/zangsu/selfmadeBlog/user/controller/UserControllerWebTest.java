e5e18324755f9010008e8456f790455109e997ed
zangsu <zangsu_@naver.com>
2024-01-07 18:08:58 +0900

    fix : `LoginService` 모킹

--- src/test/java/zangsu/selfmadeBlog/user/controller/UserControllerWebTest.java
+++ src/test/java/zangsu/selfmadeBlog/user/controller/UserControllerWebTest.java
@@ -1,25 +1,17 @@
 package zangsu.selfmadeBlog.user.controller;
 
-import org.junit.jupiter.api.BeforeEach;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Import;
-import org.springframework.stereotype.Component;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import zangsu.selfmadeBlog.user.controller.model.WebUser;
-import zangsu.selfmadeBlog.user.controller.validator.WebUserValidator;
+import zangsu.selfmadeBlog.user.service.LoginService;
 import zangsu.selfmadeBlog.user.service.UserService;
-
-import static org.assertj.core.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest
 class UserControllerWebTest {
@@ -27,14 +19,18 @@ class UserControllerWebTest {
     public static final String BINDING_RESULT_KEY = "org.springframework.validation.BindingResult.webUser";
     @Autowired
     MockMvc mvc;
+
     @MockBean
     UserService userService;
+
+    @MockBean
+    LoginService loginService;
 
     @Autowired
     UserController userController;
 
     @Test
-    public void 빈값은_허용하지_않음() throws Exception{
+    public void 빈값은_허용하지_않음() throws Exception {
         //given
         WebUser blankUser = new WebUser("", "", "");
         makeRequest(blankUser)
@@ -45,8 +41,9 @@ class UserControllerWebTest {
 
         //then
     }
+
     @Test
-    public void 공백은_허용하지_않음() throws Exception{
+    public void 공백은_허용하지_않음() throws Exception {
         //given
         WebUser blankUser = new WebUser(" ", " ", " ");
         makeRequest(blankUser)
@@ -59,7 +56,7 @@ class UserControllerWebTest {
     }
 
     @Test
-    public void id는_숫자로_시작하면_안됨() throws Exception{
+    public void id는_숫자로_시작하면_안됨() throws Exception {
         WebUser user = new WebUser("", "123User", "");
         makeRequest(user)
                 .andExpect(model().attributeHasFieldErrorCode("webUser", "userId", "Pattern"));
@@ -67,67 +64,66 @@ class UserControllerWebTest {
 
 
     @Test
-    public void id는_최소_5글자() throws Exception{
+    public void id는_최소_5글자() throws Exception {
         //given
 
-        WebUser user = new WebUser("","aaaa","");
+        WebUser user = new WebUser("", "aaaa", "");
         makeRequest(user)
                 .andExpect(model().attributeHasFieldErrorCode("webUser", "userId", "Pattern"));
     }
 
     @Test
-    public void id는_최대_10글자() throws Exception{
-        WebUser user = new WebUser("","aaaaaaaaaaa","");
+    public void id는_최대_10글자() throws Exception {
+        WebUser user = new WebUser("", "aaaaaaaaaaa", "");
         makeRequest(user)
                 .andExpect(model().attributeHasFieldErrorCode("webUser", "userId", "Pattern"));
 
     }
 
     @Test
-    public void id에_특수문자_금지() throws Exception{
+    public void id에_특수문자_금지() throws Exception {
         //given
-        WebUser user = new WebUser("","aaaa!","");
+        WebUser user = new WebUser("", "aaaa!", "");
         makeRequest(user)
                 .andExpect(model().attributeHasFieldErrorCode("webUser", "userId", "Pattern"));
 
     }
 
     @Test
-    public void 비밀번호는_특수문자_필수() throws Exception{
-        WebUser user = new WebUser("","aaaaaaaaaaa","password12");
+    public void 비밀번호는_특수문자_필수() throws Exception {
+        WebUser user = new WebUser("", "aaaaaaaaaaa", "password12");
         makeRequest(user)
                 .andExpect(model().attributeHasFieldErrorCode("webUser", "password", "Pattern"));
 
     }
 
     @Test
-    public void 비밀번호는_최소_8글자() throws Exception{
-        WebUser user = new WebUser("","aaaaaaaaaaa","pass12!");
+    public void 비밀번호는_최소_8글자() throws Exception {
+        WebUser user = new WebUser("", "aaaaaaaaaaa", "pass12!");
+        makeRequest(user)
+                .andExpect(model().attributeHasFieldErrorCode("webUser", "password", "Pattern"));
+    }
+
+    @Test
+    public void 비밀번호는_최대_20글자() throws Exception {
+        WebUser user = new WebUser("", "aaaaaaaaaaa", "password12345678pass!");
         makeRequest(user)
                 .andExpect(model().attributeHasFieldErrorCode("webUser", "password", "Pattern"));
 
     }
 
     @Test
-    public void 비밀번호는_최대_20글자() throws Exception{
-        WebUser user = new WebUser("","aaaaaaaaaaa","password12345678pass!");
-        makeRequest(user)
-                .andExpect(model().attributeHasFieldErrorCode("webUser", "password", "Pattern"));
+    public void 올바른_입력() throws Exception {
 
-    }
-
-    @Test
-    public void 올바른_입력() throws Exception{
-
-        WebUser user = new WebUser("유저","User12","password12!");
+        WebUser user = new WebUser("유저", "User12", "password12!");
         makeRequest(user)
                 .andExpect(model().hasNoErrors());
     }
 
     private ResultActions makeRequest(WebUser webUser) throws Exception {
         return mvc.perform(post("/user/join")
-                        .param("userName", webUser.getUserName())
-                        .param("userId", webUser.getUserId())
-                        .param("password", webUser.getPassword()));
+                .param("userName", webUser.getUserName())
+                .param("userId", webUser.getUserId())
+                .param("password", webUser.getPassword()));
     }
 }
